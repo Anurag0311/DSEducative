f3788f83599bd89d37943387a06f149c53134b4e
Anurag <anuragsingh.anurag1234@gmail.com>
2023-07-13 20:34:05 +0530

    Commit

---
+++ Trie/numWords.java
@@ -0,0 +1,19 @@
+package com.anurag.Trie;
+
+//Find All of the Words in a Trie
+
+public class numWords {
+
+	public static int totalWords(TrieNode root){
+	    int result = 0;
+
+	    // Leaf denotes end of a word
+	    if (root.isEndWord)
+	      result++;
+
+	    for (int i = 0; i < 26; i++)
+	      if (root.children[i] != null)
+	        result += totalWords(root.children[i]);
+	    return result;
+	  }
+}
